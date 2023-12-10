a8a52a03bde357c99d848b9f187cb2019166d2ea
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-10 20:29:37 +0900

    02 : 회원관리예제 - 회원서비스

---
+++ study_01introduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,42 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);  //중복 회원 검증
+        // 통화하면 저장
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        // 값이 있으면 IllegalStateException 메시지 출력
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
