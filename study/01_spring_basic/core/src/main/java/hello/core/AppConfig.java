44c3e0dd639a632a522d321eae10ff9cfd641768
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-26 01:16:23 +0900

    스프링 핵심원리이해2 - AppConfig 리팩터링

--- study/01_spring_basic/core/src/main/java/hello/core/AppConfig.java
+++ study/01_spring_basic/core/src/main/java/hello/core/AppConfig.java
@@ -13,11 +13,21 @@ import hello.core.order.OrderServiceImpl;
  */
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        //return new MemberServiceImpl(new MemoryMemberRepository());
+        // 역할과 구현 클래스를 한분에 보기위해 분리
+        return new MemberServiceImpl(memberRepository());
     }
+
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+        //return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private static FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
