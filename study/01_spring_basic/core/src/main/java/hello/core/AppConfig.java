ce3c239b65d175687a60294a52256545a36233fc
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-26 01:25:24 +0900

    스프링 핵심원리이해2 - 새로운 구조와 할인정잭 적용

--- study/01_spring_basic/core/src/main/java/hello/core/AppConfig.java
+++ study/01_spring_basic/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -18,16 +21,21 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository());
     }
 
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() {
         //return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private static MemoryMemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+
+
     }
 
-    private static FixDiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
-    }
+
 }
