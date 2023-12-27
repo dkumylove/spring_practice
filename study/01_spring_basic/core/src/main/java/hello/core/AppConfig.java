6febf5930231fd19b28c78a662355c96ef1ad8ad
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-28 02:16:32 +0900

    싱글톤 컨테이너 - @Configuration과 바이트코드 조작

--- study/01_spring_basic/core/src/main/java/hello/core/AppConfig.java
+++ study/01_spring_basic/core/src/main/java/hello/core/AppConfig.java
@@ -42,7 +42,7 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository());
     }
 
-    @Bean
+    @Bean  //이 붙은 메서드마다 이미 스프링 빈이 존재하면 존재하는 빈을 반환하고, 스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다
     public MemberRepository memberRepository() {
         //2번? 3번?
         System.out.println("call AppConfig.memberRepository");
@@ -63,9 +63,20 @@ public class AppConfig {
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
-
-
     }
 
+    /*
+    // AppConfig@CGLIB 예상 코드
+    @Bean
+    public MemberRepository memberRepository() {
+
+        if (memoryMemberRepository가 이미 스프링 컨테이너에 등록되어 있으면?) {
+            return 스프링 컨테이너에서 찾아서 반환;
+        } else { //스프링 컨테이너에 없으면
+            기존 로직을 호출해서 MemoryMemberRepository를 생성하고 스프링 컨테이너에 등록
+            return 반환
+        }
+    }
+     */
 
 }
